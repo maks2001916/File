1860cc94cc8ad9a361041e101e633ec73ceea23c
maks2001916 <mamonov.maksim.20003@gmail.com>
2024-11-21 18:19:20 +0500

    Написан метод записывающий в файл последовательность чётных чисел

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,37 @@
 package org.example
 
+import java.io.File
+import java.io.FileWriter
+
 fun main() {
-    println("Hello World!")
+
+    var filePath = "/home/deck/Documents/"
+    var name = "arrayNumbers"
+    var number = 17
+    var file = File(filePath+"text")
+    var fileTwo = File(filePath+name)
+    recordFile(file, "запись и чтение прошли успешно")
+    println(readFile("text", filePath))
+
+    writingPositiveEvenNumbers(fileTwo, number)
+    println(readFile(name, filePath))
+}
+
+fun writingPositiveEvenNumbers(file: File, num: Int) {
+    for (i in 0..num) {
+        if (i%2==0) {
+            recordFile(file, i.toString()+",")
+        }
+    }
+}
+
+fun recordFile(file: File, string: String) {
+    var fileWriter = FileWriter(file, true)
+    fileWriter.write(string)
+    fileWriter.close()
+}
+
+fun readFile(name: String, path: String): String {
+    var file = File(path+name)
+    return file.readText()
 }
